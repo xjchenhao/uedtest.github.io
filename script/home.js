f7ac996a946bbfea64fd8b9c90881278cb40a107
xjchenhao <wy.chenhao@qq.com>
2016-01-18 11:33:54 +0800

    页面优化

--- script/home.js
+++ script/home.js
@@ -2,6 +2,7 @@ window.onload = function () {
     //--------------------------------------------------------【判断设备修改样式】
     var userAgent = navigator.userAgent,
         regMobileDevice = /(phone|pod|iPhone|iPod|ios|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i;
+
     if (!userAgent.match(regMobileDevice)) {
         document.getElementsByTagName('body')[0].classList.remove('mobile');
     }
